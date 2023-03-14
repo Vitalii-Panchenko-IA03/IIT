568dddd57dc511be1eec4af9e01ff000f00dc1bf
Vitalii-Panchenko-IA03 <114138027+Vitalii-Panchenko-IA03@users.noreply.github.com>
2023-03-14 15:02:35 +0200

    Create sendMessage.py

---
+++ sendMessage.py
@@ -0,0 +1,7 @@
+import telegram
+
+TOKEN = '5735198574:AAGMSR1z21d1K5b8VuE8DfFotgrPf6ud3Qg'
+
+bot = telegram.Bot(TOKEN)
+
+bot.send_message(chat_id='470281358', text="I don't want to live please kill me")
